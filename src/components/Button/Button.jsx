c725615bc624bc7222e93bb73feeb7370b73c02a
Git0-Map1 <git0map0@gmail.com>
2026-06-28 00:43:57 +0545

    removing unnecessary files

--- src/components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import "./Button.css";
-function Button({ children, variant, size }) {
-  const classes =
-    `btn btn--${variant} ${size === "small" ? "btn--small" : ""}`.trim();
-  return <button className={classes}>{children}</button>;
-}
-
-export default Button;
